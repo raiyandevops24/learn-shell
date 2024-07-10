27a28ccf2fe80afff59f5d47decdd2da91873c68
raiyan <raiyan.m1201@gmail.com>
2024-07-10 03:50:50 -0400

    variable example

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+ name=DevOps
+ faculty=Raghu
+
+ echjo wlcome to $name training
+ echo $name training faculty is $faculty
+ echo ${name} training faculty is ${faculty}
+
+ apple=20
+ Apple price is - ${apple}Dollar
