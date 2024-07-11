2d6e6b793227ad8676837dbfb4f7f9a28f0741a8
raiyan <raiyan.m1201@gmail.com>
2024-07-11 01:56:47 -0400

    function & variable updated

---
+++ 04-print.sh
@@ -0,0 +1,12 @@
+# Print Color here are the color codes
+#Red        31
+#Green      32
+#Yellow     33
+#Blue       34
+#Magenta    35
+#Cyan       36
+
+# syntax : echo -e "\e[COLmMessage\e[0m"
+# -e to enable \e
+#\e to enable color
+#[COLm - which color to enable, replace COL with color 31-36
